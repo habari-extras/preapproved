202d26e3e94464891d27fc495b182a17969ed064
rickc <USER@DOMAIN>
2008-06-29 04:45:56 +0000

    Plugin: preapproved.
    1. Added configuration form so the user can set the number of approved comments the commenter must have in the database before a comment is automatically approved. This includes setting the number of approved comments to zero, so all comments are automatically approved. 
    
    2. Removed filtering from action_comment_insert_before() because the content was already filtered before we got this far.
    
    3. Added readme file.
    
    git-svn-id: http://svn.habariproject.org/habari-extras/plugins/preapproved/trunk@654 f4a90942-1786-4aa1-b092-57c31935ddc3

--- preapproved.plugin.php
+++ preapproved.plugin.php
@@ -1,19 +1,19 @@
 <?php
 
-/**
+/*
  * PreApproved Class
  * 
- * This class allows us to auto-approve previously approved commenters
+ * This class allows us to auto-approve comments
  *
- **/
+ */
 
 class PreApproved extends Plugin
 {
-	/**
+	/*
 	 * function info
 	 * Returns information about this plugin
 	 * @return array Plugin info array
-	 **/
+	 */
 	function info()
 	{
 		return array (
@@ -21,70 +21,115 @@ class PreApproved extends Plugin
 			'url' => 'http://habariproject.org/',
 			'author' => 'Habari Community',
 			'authorurl' => 'http://habariproject.org/',
-			'version' => '1.1',
-			'description' => 'Automatically approve commenters that have approved comments in the database.',
+			'version' => '1.2',
+			'description' => 'Automatically approve comments based on the number of approved comments the commenter has previously made.',
 			'license' => 'Apache License 2.0',
 		);
 	}
-	
-	/**
+
+	/*
 	 * Register the PreApproved event type with the event log
-	 */	 
-	public function action_plugin_activation( $file ) {
+	 */
+	public function action_plugin_activation( $file )
+	{
 		if ( realpath( $file ) == __FILE__ ) {
 			EventLog::register_type( 'PreApproved' );
+			if( !(Options::get( 'preapproved__approved_count' ) ) ) {
+				Options::set( 'preapproved__approved_count', 1 );
+			}
 		}
 	}
-	
-	/**
-	 * Unregister the PreApproved event type on deactivation
-	 * @todo Should we be doing this?
-	 */	 	 
-	public function action_plugin_deactivation( $file ) {
+
+	/*
+	 * Unregister the PreApproved event type on deactivation if it isn't being used
+	 */
+	public function action_plugin_deactivation( $file )
+	{
 		if ( realpath( $file ) == __FILE__ ) {
 			EventLog::unregister_type( 'PreApproved' );
 		}
 	}
-	
-	/**
+
+	public function filter_plugin_config($actions, $plugin_id)
+	{
+		if ($plugin_id == $this->plugin_id()) {
+			$actions[] = _t( 'Configure' );
+		}
+		return $actions;
+	}
+
+	public function action_plugin_ui($plugin_id, $action)
+	{
+		if ( $plugin_id == $this->plugin_id() ) {
+			switch ($action) {
+				case _t( 'Configure' ):
+					$form= new FormUI( 'preapproved' );
+					$form->append( 'text', 'approved_count', 'option:preapproved__approved_count', _t( 'Required number of approved comments: ' ) );
+					$form->approved_count->add_validator( array( $this, 'validate_integer' ) );
+					$form->append( 'submit', 'save', _t( 'Save' ) );
+					$form->out();
+				break;
+			}
+		}
+	}
+
+	/*
 	 * function act_comment_insert_before
 	 * This function is executed when the action "comment_insert_before"
 	 * is invoked from a Comment object.
 	 * The parent class, Plugin, handles registering the action 
-	 * and hook name using the name of the function to determine 
+	 * and hook name using the name of the function to determine coun
 	 * where it will be applied.
 	 * You can still register functions as hooks without using
 	 * this method, but boy, is it handy.
 	 * @param Comment The comment that will be processed before storing it in the database.
 	 * @return Comment The comment result to store.
-	 **/	 	 	 	 	
-	function action_comment_insert_before ( $comment ) {
+	 */
+	function action_comment_insert_before ( $comment )
+	{
 		// This plugin ignores non-comments
-		if( $comment->type != Comment::COMMENT ) {
-			return $comment;
+		if( $comment->type == Comment::COMMENT ) {
+			if( Comments::get( array( 'email' => $comment->email, 'name' => $comment->name, 
+								'url' => $comment->url, 'status' => Comment::STATUS_APPROVED ) )->count >= Options::get( 'preapproved__approved_count' ) ) {
+				$comment->status = Comment::STATUS_APPROVED;
+				EventLog::log( 'Comment by ' . $comment->name . ' automatically approved.', 'info', 'PreApproved', 'Preapproved' );
+			}
 		}
-		
-		// <script> is bad, mmmkay?
-		$comment->content = InputFilter::filter( $comment->content );
-
-		if( Comments::get( array( 'email' => $comment->email, 'name' => $comment->name, 
-							'url' => $comment->url, 'status' => Comment::STATUS_APPROVED ) )->count ) {
-			$comment->status = Comment::STATUS_APPROVED;
-			EventLog::log( 'Comment by ' . $comment->name . ' automatically approved.', 'info', 'comment', 'preapproved' );
-		}
-		return;
+		return $comment;
 	}
-	
-	function set_priorities() {
+
+	function set_priorities()
+	{
 	  return array( 'action_comment_insert_before' => 10 );
 	}
-	
-	/**
-     * Add update beacon support 
-     **/
-    
-    function action_update_check() {
-      Update::add( 'PreApproved', '0fa22c74-a0d6-11dc-8314-0800200c9a66', $this->info->version ); 
-    }
+
+	/*
+	* Add update beacon support 
+	*/
+	function action_update_check()
+	{
+		Update::add( 'PreApproved', '0fa22c74-a0d6-11dc-8314-0800200c9a66', $this->info->version ); 
+	}
+
+	/*
+	 * A validation function that returns an error if the value passed in is not  an integer
+	 *
+	 * @param string $value A value to test if it is an integer
+	 * @param formcontrol $control The control containing the value
+	 * @param formui $form The form containing the control
+	 * @return array An empty array if the value is an integer or an array with strings describing the errors
+	 */
+	function validate_integer( $value, $control, $form )
+	{
+		if( !ctype_digit( $value ) ) {
+			return array( _t( 'Please enter a valid positive integer.' ) );
+		}
+		$val = intval( $value );
+		if( !is_int( $val ) ) {
+			return array( _t( 'Please enter a valid positive integer.' ) );
+		}
+		return array();
+	}
+
 }
 ?>
---
+++ readme.txt
@@ -0,0 +1,40 @@
+Plugin: PreApproved
+URL: http://habariproject.org
+Version: 1.2
+Author: Habari Project
+
+Purpose 
+
+PreApproved will automatically approve comments from commenters who already have the specified number of approved comments in your blog's database.
+
+Requirements 
+
+None.
+
+Installation
+
+1. Copy the plugin directory into your user/plugins directory or the site's plugins directory.
+2. Go to the plugins page of your Habari admin panel.
+3. Click on the Activate button for Meta SEO.
+
+Usage
+
+Configuration for PreApproved is performed by clicking the Configure button in its listing on your plugins page. The configuration screen has only one field - the number of comments the commenter must already have in the database to have new comments automatically approved. If you enter the number zero in this field, all comments will be automatically approved.
+
+After you're done making your configuration changes, click the 'Save' button at the bottom of the form to save the changes. Click the 'Close' button to completely close the form.
+
+Uninstallation
+
+1. Got to the plugins page of your Habari admin panel.
+2. Click on the Deactivate button.
+3. Delete the 'preapproved' directory from your user/plugins directory.
+
+Cleanup
+
+The plugin places one item in your Options table. It is preceded by the word preapproved. You can safely delete this entry after you uninstall the plugin.
+
+Changelog
+
+Version 1.2
+Change: Added configuration dialog to the plugin.
+Change: Removed content filter from the plugin since the content is filtered before it reaches the plugin.
